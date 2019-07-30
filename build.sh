b122c5e8e0fdbd3686f3e3648fa27a63c4d70d07
Polina Shevchenko <11236361+gigikalo@users.noreply.github.com>
2019-07-30 11:54:24 +0300

    Change reference to requirements file in build

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pushd `dirname $0` > /dev/null
 echo "Build server"
 virtualenv -p python3.7 ./runtime
 source ./runtime/bin/activate
-pip install -U -r requirenments.txt
+pip install -U -r requirements.txt
 
 for i in `find src -iname "setup.py" -maxdepth 2`; do pip install -e `dirname ${i}`; done
 
