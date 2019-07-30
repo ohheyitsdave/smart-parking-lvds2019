708ea1e3e1b9f7ceed2c9074a6a125c4dc0c3f16
Michael <michael@girnyak.com>
2019-07-30 11:12:09 +0300

    up

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e              # to make script exit as soon as any command fails
+
+pushd `dirname $0` > /dev/null
+
+echo "Build server"
+virtualenv -p python3.7 ./runtime
+source ./runtime/bin/activate
+pip install -U -r requirenments.txt
+
+for i in `find src -iname "setup.py" -maxdepth 2`; do pip install -e `dirname ${i}`; done
+
+deactivate
+
+popd > /dev/null
+
