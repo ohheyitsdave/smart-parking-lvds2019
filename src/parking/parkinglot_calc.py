a7f84764b1a7d81d543ca8a2e91474857015a10a
Michael <michael@girnyak.com>
2019-08-01 22:29:43 +0300

    up

--- src/parking/parkinglot_calc.py
+++ src/parking/parkinglot_calc.py
@@ -18,7 +18,7 @@ def get_masks_for_parking_lots(pklot_config):
         img = Image.new('L', (IMAGE_SHAPE[0], IMAGE_SHAPE[1]), 0)
         vert = list(zip([float(px) for px in d['xn'].split(';')], [float(py) for py in d['yn'].split(';')]))
         ImageDraw.Draw(img).polygon(vert, fill='white')
-        yield {'id': d['id'], 'mask': np.array(img).astype(bool)}
+        yield {'id': d['id'], 'mask': np.array(img).astype(bool), 'vertices': vert}
 
 
 def calc_occupancy_level(slot_mask, cars_mask):
@@ -30,11 +30,25 @@ def calc_occupancy_level(slot_mask, cars_mask):
 if __name__ == '__main__':
     from itertools import islice
 
+    from PIL import Image, ImageDraw
+
+    img = Image.open('/Users/michael/work/lvivds/parkingslot/images/2018-07-16 07:45:39.024.jpg')
+    img.convert("RGBA")
     pklot_config = read_config()
+
+    from colour import Color
+
+    green = Color("green")
+    gradient_colors = list(green.range_to(Color("red"), 100))
 
     from pickle import load
     single_mask = load(open('temp/single_mask.pkl', 'rb'))
     for data in get_masks_for_parking_lots(pklot_config):
-        print(data['id'], calc_occupancy_level(data['mask'], single_mask))
+        confidence = calc_occupancy_level(data['mask'], single_mask)
+        print(data['id'], confidence)
+        color = tuple([int(c * 256) for c in gradient_colors[int(confidence * 100)].get_rgb()] + [125, ])
+        ImageDraw.Draw(img, 'RGBA').polygon(data['vertices'], fill=color)
         #img = Image.fromarray(data['mask'])
-        #img.show()
+
+    #img.show()
+    img.save('temp/places.jpg', 'JPEG')
